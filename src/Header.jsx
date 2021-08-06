154b17f48665d6b12428b3f575e3a69b60844fab
Ayush Kapadia <ayushkapadia619@gmail.com>
2021-08-06 17:06:30 +0530

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css"
+function Header (){
+    return <div style = {{backgroundColor: "red", textAlign: "center", fontSize:"40px"}}>
+       My Videos
+    </div>
+}
+
+export default Header
